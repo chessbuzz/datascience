02e5068711f72be9545f4a303f9311e281474114
chessbuzz <chessbuzz@gmail.com>
2021-01-18 20:51:23 -0500

    Update largecsv_to_postgres.py

--- largecsv_to_postgres.py
+++ largecsv_to_postgres.py
@@ -14,7 +14,7 @@ print(df.head())
 # engine format
 # create_engine
     #("<dbms>://<username>:<password>@<hostname>:<port>/<db_name>")
-engine = create_engine('postgresql://postgres:Password@911@website.com:5432/buildingpermits')
+engine = create_engine('postgresql://postgres:Password@website.com:5432/buildingpermits')
 
 # create an iterable that will read chunksize 1000 rows at a time
 # save the data from the dataframe to the postgres table permitinfo
